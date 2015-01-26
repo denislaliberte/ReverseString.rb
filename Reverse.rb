225aa55d04c9f7f574f33e4762d70ddfa1030108
Denis Laliberté <denis@tp1.ca>
2015-01-26 17:01:37 -0500

    get the first char last

--- Reverse.rb
+++ Reverse.rb
@@ -4,7 +4,7 @@ def reverse(input)
   if(not input or input.size ==0 )
     ""
   else
-    input[-1..-1] + "a"
+    input[-1..-1] + input[0..0]
   end
 end
 
--- Reverse_spec.rb
+++ Reverse_spec.rb
@@ -8,5 +8,6 @@ describe "reverse" do
   it "reverse two charactere string" do
     expect(reverse("ab")).to eq("ba")
     expect(reverse("ac")).to eq("ca")
+    expect(reverse("bc")).to eq("cb")
   end
 end
