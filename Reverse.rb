ecc5bbdb5ef94498f96cd947e4ffc45b77308b75
Denis Laliberté <denis@tp1.ca>
2015-01-26 15:25:47 -0500

    reverse ab

--- Reverse.rb
+++ Reverse.rb
@@ -1,6 +1,10 @@
 
 
 def reverse(input)
-  ""
+  if(not input or input.size ==0 )
+    ""
+  else
+    "b" + "a"
+  end
 end
 
--- Reverse_spec.rb
+++ Reverse_spec.rb
@@ -5,4 +5,7 @@ describe "reverse" do
   it "return an empty string" do
     expect(reverse("")).to eq("")
   end
+  it "reverse two charactere string" do
+    expect(reverse("ab")).to eq("ba")
+  end
 end
